74ea9004476a439574a83ce09e61666fd0b4431b
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-01 21:35:58 -0500

    añadiendo atajos en el header

--- bitcoin/src/components/PxHeader.vue
+++ bitcoin/src/components/PxHeader.vue
@@ -13,7 +13,16 @@
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:w-auto"
         >
-          <div class="text-sm lg:flex-grow"></div>
+          <div class="text-sm lg:flex-grow">
+            <router-link
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+              v-for="l in links"
+              :key="l.title"
+              :to="l.to"
+            >
+              {{ l.title }}
+            </router-link>
+          </div>
         </div>
       </nav>
     </nav>
@@ -22,8 +31,15 @@
 
 <script>
 import PxIcon from "@/components/PxIcon";
+
 export default {
   name: "PxHeader",
   components: { PxIcon },
+  props: {
+    links: {
+      type: Array,
+      default: () => [],
+    },
+  },
 };
 </script>
